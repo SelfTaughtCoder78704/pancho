35bcb70a8dc511d2716ee91e399a749ac0eae3a7
SelfTaughtCoder78704 <bobbynicholson78704@gmail.com>
2021-07-11 15:23:24 -0500

    adding final touches

--- main.js
+++ main.js
@@ -40,20 +40,35 @@ function showOrHideTimeLine() {
     }
 }
 
-// Get the modal
+// Get the modals
 let modal = document.getElementById("myModal");
+let vidModal = document.getElementById("myModalVid");
+
+let vidBtn = document.getElementById('vidBtn');
+
+vidBtn.addEventListener('click', showVideoModal);
+
 let imageToShow = document.getElementById('imageToShow')
 // Get the images that open the modal
 let evidenceImages = document.querySelectorAll('.imgGrid img')
 
 // Get the <span> element that closes the modal
 let span = document.getElementsByClassName("close")[0];
+let span2 = document.querySelector(".close2")
+
+let iframe = document.querySelector('#theVid');
 
 // When the user clicks on the button, open the modal
 function showModal(e) {
   modal.style.display = "block";
   imageToShow.innerHTML = ` <p>${this.getAttribute('data-text')}</p><img src="${this.src}" alt="evidence photo">`
 }
+
+function showVideoModal(e) {
+  vidModal.style.display = "block";
+  playVideo()
+}
+
 
 evidenceImages.forEach(img => {
   img.addEventListener('click', showModal)
@@ -65,10 +80,29 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
+span2.onclick = function() {
+  console.log('clicked')
+  vidModal.style.display = "none";
+  stopVideo()
+}
+
+const stopVideo = function (  ) {
+	iframe.src = ''
+};
+
+const playVideo = function() {
+  iframe.src = 'https://www.youtube.com/embed/lYFDFc8esRw'
+}
+
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  }
+  if(event.target == vidModal){
+    vidModal.style.display = "none";
+    stopVideo()
   }
 }
 
@@ -125,3 +159,4 @@ let callback = (entries, observer) => {
 let observer = new IntersectionObserver(callback, options)
 let alllazy = document.querySelectorAll('.observed')
 alllazy.forEach(vid => observer.observe(vid));
+ 
